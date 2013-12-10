97a9271208c1b16b6db640498c3d36ecae7dd31f
kxt5258 <kxt5258@gmail.com>
2013-12-10 15:47:33 +0600

    minor change to image url

--- web-app/js/OpenLayers-2.10/lib/OpenLayers/Util.js
+++ web-app/js/OpenLayers-2.10/lib/OpenLayers/Util.js
@@ -668,7 +668,7 @@ OpenLayers.Util.urlAppend = function(url, paramStr) {
  * Property: ImgPath
  * {String} Default is ''.
  */
-OpenLayers.ImgPath = '';
+OpenLayers.ImgPath = '../../img/';
 
 /** 
  * Function: getImagesLocation
